9d5728f79bc3557cd3f6962f57374c020377ba27
aliaksandr_liahushau <aliaksandr_liahushau@epam.com>
2017-02-21 17:20:48 +0300

    replace tabs with spaces; add project absolute path to user input;

--- src/com/epam/mentoring/lessone/App.java
+++ src/com/epam/mentoring/lessone/App.java
@@ -6,64 +6,67 @@ import java.util.Scanner;
 
 public class App {
 
-	final private static String CLASS_TO_LOAD = "Semaphore";
-	final private static String EXIT_COMMAND = "exit";
-	final private static String STATUS_SUCCESS = "Status: success";
-	final private static String STATUS_ERROR = "Status: error";
-	
-	final private static Scanner scanner = new Scanner(System.in, "UTF-8");
-	
-	public static void main(String[] args) {
-		run();
-	}
-	
-	private static void run() {
+    final private static String CLASS_TO_LOAD = "Semaphore";
+    final private static String EXIT_COMMAND = "exit";
+    final private static String STATUS_SUCCESS = "Status: success";
+    final private static String STATUS_ERROR = "Status: error";
+
+    final private static Scanner scanner = new Scanner(System.in, "UTF-8");
+
+    public static void main(String[] args) {
+        run();
+    }
+
+    private static void run() {
+        final String projectAbsolutePath = System.getProperty("user.dir");
         boolean run = true;
         System.out.println("Welcome to custom classloader");
+        System.out.println("Please enter path in project folder to load class from or 'exit' to close the programm.");
         while(run) {
-        	System.out.println("Please enter path to load from or 'exit' to close the programm");
-        	final String input = scanner.nextLine();
-        	if (input.equals(EXIT_COMMAND)) {
+            System.out.println("");
+            System.out.println("Enter path:");
+            final String input = scanner.nextLine();
+            if (input.equals(EXIT_COMMAND)) {
                 run = false;
-            } else if (!input.isEmpty()) {
-            	System.out.println("");
-            	load(input);
+            } else {
+                System.out.println("");
+                load(projectAbsolutePath + "\\" + input + "\\");
             }
-		}
+        }
     }
-	
-	private static void load(final String path) {
-		System.out.println("Status: start loading...");
-		final CustomLoader loader = new CustomLoader(path, ClassLoader.getSystemClassLoader());
-		
-		try {
-			Class<?> clazz = loader.loadClass(CLASS_TO_LOAD);
-			System.out.println(STATUS_SUCCESS);
-			Method m = clazz.getMethod("lever");
-			Object obj = clazz.newInstance();
-			m.invoke(obj);
-		} catch (ClassNotFoundException e) {
-			System.out.println(STATUS_ERROR);
-			e.printStackTrace(System.out);
-		} catch (NoSuchMethodException e) {
-			System.out.println(STATUS_ERROR);
-			e.printStackTrace(System.out);
-		} catch (SecurityException e) {
-			System.out.println(STATUS_ERROR);
-			e.printStackTrace(System.out);
-		} catch (InstantiationException e) {
-			System.out.println(STATUS_ERROR);
-			e.printStackTrace(System.out);
-		} catch (IllegalAccessException e) {
-			System.out.println(STATUS_ERROR);
-			e.printStackTrace(System.out);
-		} catch (IllegalArgumentException e) {
-			System.out.println(STATUS_ERROR);
-			e.printStackTrace(System.out);
-		} catch (InvocationTargetException e) {
-			System.out.println(STATUS_ERROR);
-			e.printStackTrace(System.out);
-		}
-	} 
+
+    private static void load(final String path) {
+        System.out.println("Status: start loading...");
+        final CustomLoader loader = new CustomLoader(path, ClassLoader.getSystemClassLoader());
+
+        try {
+            Class<?> clazz = loader.loadClass(CLASS_TO_LOAD);
+            System.out.println(STATUS_SUCCESS);
+            Method m = clazz.getMethod("lever");
+            Object obj = clazz.newInstance();
+            m.invoke(obj);
+        } catch (ClassNotFoundException e) {
+            System.out.println(STATUS_ERROR);
+            e.printStackTrace(System.out);
+        } catch (NoSuchMethodException e) {
+            System.out.println(STATUS_ERROR);
+            e.printStackTrace(System.out);
+        } catch (SecurityException e) {
+            System.out.println(STATUS_ERROR);
+            e.printStackTrace(System.out);
+        } catch (InstantiationException e) {
+            System.out.println(STATUS_ERROR);
+            e.printStackTrace(System.out);
+        } catch (IllegalAccessException e) {
+            System.out.println(STATUS_ERROR);
+            e.printStackTrace(System.out);
+        } catch (IllegalArgumentException e) {
+            System.out.println(STATUS_ERROR);
+            e.printStackTrace(System.out);
+        } catch (InvocationTargetException e) {
+            System.out.println(STATUS_ERROR);
+            e.printStackTrace(System.out);
+        }
+    } 
 
 }
